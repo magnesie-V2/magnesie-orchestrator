a016b57cd0f54f90a0991cb54c049d607e400aee
Marc LAMY <marclamy.bzh@gmail.com>
2020-11-17 11:28:28 +0100

    Basic test of cluster instantiating

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+pub mod cluster;
+
+use cluster::Cluster::*;
+use cluster::Grid5000::*;
+
 fn main() {
-    println!("Hello, world!");
+    let g5k: Grid5000 = Grid5000::new(String::from("https://www.cluster.io/"));
+
+    let g5kResponse = g5k.has_green_energy_available();
+
+    println!("g5k green energy : {r}", r = g5kResponse);
 }
