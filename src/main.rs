d412b8d86ade8a2bc2dc0115ae30f878e3926067
Marc LAMY <marclamy.bzh@gmail.com>
2020-11-17 12:40:04 +0100

    Added main as bin in Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-pub mod cluster;
+pub mod clusters;
 
-use cluster::Cluster::*;
-use cluster::Grid5000::*;
+use clusters::cluster::*;
+use clusters::grid5000::*;
 
 fn main() {
-    let g5k: Grid5000 = Grid5000::new(String::from("https://www.cluster.io/"));
+    let g5k: Grid5000 = Grid5000::new(String::from("https://api.grid5000.fr/3.0/?pretty"));
 
     let g5kResponse = g5k.has_green_energy_available();
 
