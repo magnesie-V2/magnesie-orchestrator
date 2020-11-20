a3ffe5a92f9566251f29d07ef8b7f74bce166b72
bart-tek <barthelemy.tek@imt-atlantique.net>
2020-11-20 11:26:45 +0100

    deploy wip 2

--- src/bin/Grid5000_client.rs
+++ src/bin/Grid5000_client.rs
@@ -4,6 +4,8 @@ extern crate serde;
 use std::collections::HashMap;
 use serde::{Serialize, Deserialize};
 use std::env;
+use std::fs;
+use std::{thread, time};
 
 #[derive(Deserialize, Debug)]
 struct JobSubmitResponse {
@@ -34,6 +36,14 @@ struct LinkJob {
     r#type : String,
 }
 
+#[derive(Serialize, Debug)]
+struct DeploymentRequest {
+    environment : String,
+    nodes : Vec<String>,
+    ssh_key : String
+}
+
+#[allow(unused_must_use)]
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -41,9 +51,15 @@ fn main() {
     let password : &str = &args[2];
     let nb_nodes : &str = &args[3];
     let walltime : &str = &args[4];
+    let env = "wheezy-x64-base";
+    let ssh_key_path : &str = &args[5];
     
-    let job : JobSubmitResponse = reserve_node(username, password, nb_nodes, walltime).unwrap();
-    delete_job(username, password, job.uid.to_string());
+    let ssh_key : String = get_ssh_key(ssh_key_path).unwrap();
+    
+    let job_waiting : JobSubmitResponse = reserve_node(username, password, nb_nodes, walltime).unwrap();
+    let job_deployed : JobSubmitResponse = get_reservation(username, password, job_waiting.uid.to_string()).unwrap();
+    deploy_env_on_node(username, password, job_deployed.assigned_nodes, env, ssh_key.as_str());
+    // delete_job(username, password, job.uid.to_string());
     // get_grid5000(username, password);
 }
 
@@ -58,15 +74,30 @@ fn reserve_node(username : &str, password : &str, nb_nodes : &str, walltime : &s
 
     let client = reqwest::blocking::Client::new();
 
-
     let res = client.post(api_url)
                                  .json(&map)
                                  .basic_auth(username, Some(password))
                                  .send()
                                  .expect("Failed to send request");
 
-    println!("Status: {}", res.status());
-    println!("Headers:\n{:#?}", res.headers());
+    let response_body : JobSubmitResponse = res.json().unwrap();
+    println!("{:?}", response_body);
+
+    Ok(response_body)
+}
+
+fn get_reservation(username : &str, password : &str, job_uid : String) -> Result<JobSubmitResponse, reqwest::Error> {
+
+    thread::sleep(time::Duration::from_secs(5));
+
+    let api_url = "https://api.grid5000.fr/3.0/sites/rennes/jobs/";
+
+    let client = reqwest::blocking::Client::new();
+    let res = client.get(format!("{}{}", api_url, job_uid).as_str())
+                    .basic_auth(username, Some(password))
+                    .send()
+                    .expect("Failed to send request");
+
 
     let response_body : JobSubmitResponse = res.json().unwrap();
     println!("{:?}", response_body);
@@ -75,20 +106,20 @@ fn reserve_node(username : &str, password : &str, nb_nodes : &str, walltime : &s
 }
 
 
-
-/*fn deploy_env_on_node(username : &str, password : &str, nodes : Vec<&str>, environment : &str, ssh_key : &str) -> Result<(), reqwest::Error>  {
+fn deploy_env_on_node(username : &str, password : &str, nodes : Vec<String>, environment : &str, ssh_key : &str) -> Result<(), reqwest::Error>  {
 
     let api_url = "https://api.grid5000.fr/3.0/sites/rennes/deployments";
 
-    let mut map = HashMap::new();
-    map.insert("nodes", serde_json::to_string(&nodes).unwrap());
-    map.insert("environment", environment);
-    map.insert("key", ssh_key);
+    let request_body  = DeploymentRequest {
+        nodes : nodes,
+        environment : environment.to_string(),
+        ssh_key : ssh_key.to_string()
+    };
 
     let client = reqwest::blocking::Client::new();
 
     let res = client.post(api_url)
-                                 .json(&map)
+                                 .json(&request_body)
                                  .basic_auth(username, Some(password))
                                  .send()
                                  .expect("Failed to send request");
@@ -98,8 +129,9 @@ fn reserve_node(username : &str, password : &str, nb_nodes : &str, walltime : &s
     println!("Body:\n{:#?}", res.text());
 
     Ok(())
-}*/
+}
 
+#[allow(dead_code)]
 fn get_grid5000(username : &str, password : &str) -> Result<(), reqwest::Error> {
     
     let client = reqwest::blocking::Client::new();
@@ -115,6 +147,7 @@ fn get_grid5000(username : &str, password : &str) -> Result<(), reqwest::Error> 
     Ok(())
 }
 
+#[allow(dead_code)]
 fn delete_job(username : &str, password : &str, job_to_delete : String) -> Result<(), reqwest::Error> {
 
     let api_url = "https://api.grid5000.fr/3.0/sites/rennes/jobs/";
@@ -133,4 +166,9 @@ fn delete_job(username : &str, password : &str, job_to_delete : String) -> Resul
     println!("Body:\n{}", response_body);
                     
     Ok(())
+}
+
+fn get_ssh_key(file_path : &str) -> Result<String, Box<dyn std::error::Error + 'static>> {
+    let ssh_key: String = fs::read_to_string(file_path)?;
+    Ok(ssh_key)
 }
