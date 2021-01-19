cf78db5cd8466c239d0a328922eb798f4a0e9d5d
Marc LAMY <marclamy.bzh@gmail.com>
2021-01-19 23:20:10 +0100

    Set the photogrammetry service default IP to docker's gateway

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ fn main() {
     let mut services_keeper = ServicesKeeper::new();
 
     let photogrammetry_access_info = ServiceAccessInformation::new(
-        String::from(String::from("localhost")), // 8645cc99-fdca-4a6a-bf45-1eb639a54f2c.mock.pstmn.io
-        80,
+        String::from(String::from("172.17.0.1")), // 8645cc99-fdca-4a6a-bf45-1eb639a54f2c.mock.pstmn.io
+        7979,
         String::from(""),
         String::from(""),
     );
