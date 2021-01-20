aceb458040e8707ac5b89ddfb0c66b547cc3dd9c
Marc LAMY <marclamy.bzh@gmail.com>
2021-01-20 11:51:38 +0100

    Allowed several readers of the services keeper by replacing Mutex with RwLock

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 mod services;
 
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, RwLock};
 
 use services::photogrammetry_service::PhotogrammetryService;
 use services::services_keeper::ServicesKeeper;
 use services::service_access_information::ServiceAccessInformation;
-use crate::services::service_error::ServiceError;
 
 #[allow(dead_code)]
 fn main() {
-    let services_keeper = Arc::new(Mutex::new(ServicesKeeper::new()));
+    let services_keeper = Arc::new(RwLock::new(ServicesKeeper::new()));
 
     let photogrammetry_access_info = ServiceAccessInformation::new(
-        String::from(String::from("8645cc99-fdca-4a6a-bf45-1eb639a54f2c.mock.pstmn.io")), // 8645cc99-fdca-4a6a-bf45-1eb639a54f2c.mock.pstmn.io           172.17.0.1
-        80,
+        String::from(String::from("172.17.0.1")), // 8645cc99-fdca-4a6a-bf45-1eb639a54f2c.mock.pstmn.io           172.17.0.1
+        7979, // 80             7979
         String::from(""),
         String::from(""),
     );
+    // let photogrammetry_service = PhotogrammetryService::new(services_keeper.clone());
 
-    services_keeper.lock().unwrap().register_service("photogrammetry", photogrammetry_access_info);
+    services_keeper.write().unwrap().register_service("photogrammetry", photogrammetry_access_info);
 
     match PhotogrammetryService::test(services_keeper.clone()) {
         Ok(_) => {}
