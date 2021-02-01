f4751f85cff7d4f4be0c2ceed8cd489bd0347ab4
Maël Gaonach <mael@gaonach.com>
2021-02-01 09:31:59 +0100

    grid5000 green energy mock

--- src/clusters/grid5000.rs
+++ src/clusters/grid5000.rs
@@ -1,13 +1,20 @@
-use crate::Cluster;
+use chrono::{Utc, Timelike};
+use crate::clusters::cluster::Cluster;
 
 pub struct Grid5000;
 
-impl Cluster for Grid5000 {
-    fn new(_: std::string::String) -> Self {
-        todo!()
+impl Grid5000 {
+    pub fn new(_: &str) -> Grid5000 {
+        Grid5000 {}
     }
 
-    fn has_green_energy_available(self) -> bool {
-        todo!()
+    pub fn has_green_energy_available(self) -> bool {
+        let now = Utc::now();
+        let minute = now.minute();
+        return if minute % 2 == 0 {
+            true
+        } else {
+            false
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod services;
+mod clusters;
 
 use services::service_access_information::*;
 use services::photogrammetry_service::*;
+use crate::clusters::grid5000::Grid5000;
+use crate::clusters::cluster::Cluster;
 
 #[tokio::main]
 async fn main() {
 
-    let photogrammetry_access_info = ServiceAccessInformation::new(
+    /*let photogrammetry_access_info = ServiceAccessInformation::new(
         String::from("myUrl"),
         8080,
         String::from(""),
@@ -15,6 +18,9 @@ async fn main() {
 
     let photogrammetry_service = PhotogrammetryService::new(photogrammetry_access_info);
 
-    photogrammetry_service.print_access_info();
+    photogrammetry_service.print_access_info();*/
+
+    let cluster = Grid5000::new("test");
+    println!("{}",cluster.has_green_energy_available());
 }
 
