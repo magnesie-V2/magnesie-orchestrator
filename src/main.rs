eaa23f347c30cc7017fcea1f945f77a0a3f06e36
Marc LAMY <marclamy.bzh@gmail.com>
2021-02-02 17:29:03 +0100

    Cleaned the main.rs file

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use std::sync::{Arc, RwLock};
 
 use services::{PhotogrammetryService, ServicesKeeper};
 use jobs_buffer::{JobsBuffer};
-use crate::services::InputService;
+use crate::services::ImageStorageService;
 
 fn main() -> Result<(), String>{
     let services_keeper = Arc::new(RwLock::new(ServicesKeeper::new()));
     let jobs_buffer = Arc::new(RwLock::new(JobsBuffer::new()));
 
-    let input_service = InputService::new(services_keeper.clone())?;
+    let image_storage_service = ImageStorageService::new(services_keeper.clone())?;
     let photogrammetry_service = PhotogrammetryService::new(services_keeper.clone())?;
 
 
