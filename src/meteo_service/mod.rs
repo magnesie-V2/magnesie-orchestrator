1c324910b9c3e0a8d9433fdeb7d34aa3d57c4cbb
bart-tek <barthelemy.tek@imt-atlantique.net>
2021-02-24 18:14:10 +0100

    doc

--- src/meteo_service/mod.rs
+++ src/meteo_service/mod.rs
@@ -1,2 +1,4 @@
+//! Contains a client to make REST request to the OpenWeatherMap API to get weather data in different cities
+
 pub mod meteo_service;
 pub use meteo_service::*;
--- src/ssh_client/mod.rs
+++ src/ssh_client/mod.rs
@@ -1,2 +1,4 @@
+//! Contains a client to perform a SSH connection and SSH commands on remote host
+
 mod ssh_client;
 pub use ssh_client::SshClient;
